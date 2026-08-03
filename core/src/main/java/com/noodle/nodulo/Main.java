a9f6a963944c382756328fa48786037c076b9fa7
akariumfish <akariumfish@gmail.com>
2026-08-03 21:21:51 +0200

    asset

--- core/src/main/java/com/noodle/nodulo/Main.java
+++ core/src/main/java/com/noodle/nodulo/Main.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-    @Override
-    public void create() {
-        setScreen(new FirstScreen());
-    }
+	@Override
+	public void create() {
+		FirstScreen2 fscreen = new FirstScreen2();
+		fscreen.setup();
+		setScreen(fscreen);
+	}
 }
